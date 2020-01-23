aa41135cdb1c0ec55b8fa54a28b4831e2fb2b0c3
Artenes Nogueira <artenesama@gmail.com>
2020-01-22 20:36:01 -0400

    Saves and restore state fo DateTextInputEditText
    also were done some extra things
    - add string for title in date picker
    - standardize creation of calendar instance
    - make picker instance not property to set its initial selection

--- app/src/main/java/bok/artenes/budgetcontrol/DateTextInputEditText.kt
+++ app/src/main/java/bok/artenes/budgetcontrol/DateTextInputEditText.kt
@@ -1,6 +1,8 @@
 package bok.artenes.budgetcontrol
 
 import android.content.Context
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.View
@@ -14,10 +16,7 @@ import java.util.*
 class DateTextInputEditText : TextInputEditText, View.OnClickListener,
     MaterialPickerOnPositiveButtonClickListener<Long> {
 
-    //TODO: state is not preserved
     private lateinit var _date: Calendar
-
-    private lateinit var picker: MaterialDatePicker<Long>
 
     var date: Calendar
         get() = _date
@@ -32,9 +31,7 @@ class DateTextInputEditText : TextInputEditText, View.OnClickListener,
     constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
         setFocusable(false)
         setOnClickListener(this)
-        date = Calendar.getInstance()
-        picker = makePicker()
-        picker.addOnPositiveButtonClickListener(this)
+        date = makeUTCCalendar(System.currentTimeMillis())
     }
 
     private fun setFormattedDateOnField(calendar: Calendar) {
@@ -49,24 +46,74 @@ class DateTextInputEditText : TextInputEditText, View.OnClickListener,
     }
 
     override fun onClick(v: View?) {
+        val picker = makePicker(date.timeInMillis)
+        picker.addOnPositiveButtonClickListener(this)
         val fragmentManager =
             ((context as ContextThemeWrapper).baseContext as AppCompatActivity).supportFragmentManager
         picker.show(fragmentManager, DATE_PICKER_FRAGMENT_TAG)
     }
 
     override fun onPositiveButtonClick(selection: Long) {
-        val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
-        calendar.timeInMillis = selection
-        date = calendar
+        date = makeUTCCalendar(selection)
     }
 
-    private fun makePicker(): MaterialDatePicker<Long> {
+    override fun onSaveInstanceState(): Parcelable? {
+        val superState = super.onSaveInstanceState() as Parcelable
+        val state = SavedState(superState)
+        state.timestamp = _date.timeInMillis
+        return state
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        val savedState = state as SavedState
+        super.onRestoreInstanceState(savedState.superState)
+        date = makeUTCCalendar(savedState.timestamp)
+    }
+
+    private fun makePicker(initialTime: Long): MaterialDatePicker<Long> {
         val builder = MaterialDatePicker.Builder.datePicker()
-        builder.setSelection(MaterialDatePicker.thisMonthInUtcMilliseconds())
+        builder.setSelection(initialTime)
+        builder.setTitleText(R.string.select_a_date)
         val typedValue = TypedValue()
         context.theme.resolveAttribute(R.attr.materialCalendarFullscreenTheme, typedValue, true)
         builder.setTheme(typedValue.data)
         return builder.build()
+    }
+
+    private fun makeUTCCalendar(timestamp: Long): Calendar {
+        val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+        calendar.clear()
+        calendar.timeInMillis = timestamp
+        return calendar
+    }
+
+    private class SavedState : BaseSavedState {
+
+        var timestamp: Long = 0
+
+        constructor(parcelable: Parcelable) : super(parcelable)
+
+        constructor(parcelIn: Parcel) : super(parcelIn) {
+            timestamp = parcelIn.readLong()
+        }
+
+        override fun writeToParcel(out: Parcel, flags: Int) {
+            super.writeToParcel(out, flags)
+            out.writeLong(timestamp)
+        }
+
+        @JvmField
+        val CREATOR: Parcelable.Creator<SavedState> = object : Parcelable.Creator<SavedState> {
+            override fun createFromParcel(`in`: Parcel): SavedState {
+                return SavedState(`in`)
+            }
+
+            @Suppress("UNCHECKED_CAST")
+            override fun newArray(size: Int): Array<SavedState> {
+                return arrayOfNulls<SavedState>(size) as Array<SavedState>
+            }
+        }
+
     }
 
     companion object {
