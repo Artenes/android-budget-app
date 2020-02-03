cfc702870b5b70a10f11bb9a7e61396ddfb0a9cf
Artenes Nogueira <artenesama@gmail.com>
2020-02-03 19:27:19 -0400

    Move account view fragment to activity

--- app/src/main/java/bok/artenes/budgetcontrol/account/AccountViewActivity.kt
+++ app/src/main/java/bok/artenes/budgetcontrol/account/AccountViewActivity.kt
@@ -4,41 +4,50 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentFactory
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import bok.artenes.budgetcontrol.R
+import kotlinx.android.synthetic.main.activity_account_view.*
 
 class AccountViewActivity : AppCompatActivity() {
 
+    private val accountUid: String?
+        get() = intent?.extras?.getString(EXTRA_UID)
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        val id = intent?.extras?.getString(EXTRA_ID)
-        val factory = FragmentViewFactory(id)
-        supportFragmentManager.fragmentFactory = factory
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_view)
+
+        val factory = AccountViewViewModel.Factory(accountUid)
+        val viewModel = ViewModelProviders.of(this, factory).get(AccountViewViewModel::class.java)
+
+        viewModel.account.observe(this, Observer {
+            textInputName.setText(it.name)
+            textInputBalance.setText(it.balance)
+        })
+
+        viewModel.saveFinished.observe(this, Observer {
+            if (it) {
+                finish()
+            }
+        })
+
+        buttonSave.setOnClickListener {
+            viewModel.save(
+                textInputName.text.toString(),
+                textInputBalance.value
+            )
+        }
     }
 
     companion object {
 
-        private const val EXTRA_ID = "ID"
+        private const val EXTRA_UID = "UID"
 
         fun start(context: Context, id: String? = null) {
             val intent = Intent(context, AccountViewActivity::class.java)
-            intent.putExtra(EXTRA_ID, id)
+            intent.putExtra(EXTRA_UID, id)
             context.startActivity(intent)
-        }
-
-    }
-
-    class FragmentViewFactory(val id: String?) : FragmentFactory() {
-
-        override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
-            val clazz = loadFragmentClass(classLoader, className)
-            return if (clazz == AccountViewFragment::class.java) {
-                AccountViewFragment(id)
-            } else {
-                super.instantiate(classLoader, className)
-            }
         }
 
     }
